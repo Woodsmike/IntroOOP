5e30f7425a38fe5842fa11f508002db686253e2f
Woodsmike <Woodsmike@users.noreply.github.com>
2017-07-10 14:30:33 -0400

    Added cat class

--- IntroToOOP/Program.cs
+++ IntroToOOP/Program.cs
@@ -10,6 +10,26 @@ namespace IntroToOOP
     {
         static void Main(string[] args)
         {
+            //When we initialize a new object, it is called instantiating 
+            //creating a new instance of that object.
+            //An object needs to be instantiated before it can be used.
+            //All objects of a specific type are created from the same template - CLASS
+            //All objects from a class will have the same number and type of states,
+            //But each instance can have it's own unique values.
+
+            //format we use to instantiate an object is:
+            //ClassName objectName = new ClassName();
+            Random rName = new Random();
+            Cat catName = new Cat();
+            //To create a new object, we use a special method called a CONSTRUCTOR  
+            //Thinking about objects in real life, create 3 examples of objects.
+            //Your objects should have five states (characteristics) and 5 behaviours.
+            //house - living spaces, windows, doors, deck, driveway
+                    //- heats/cools, if mobile can move, protects family
+            //wife- beautiful, smart, good looking, marginally tall, blue eyes
+                    //-witty, tells me what to do, loves
+            //ping pong ball - round, lightweight, white, small, hollow
+                    //-moves, illusive, bounces
         }
     }
 }
